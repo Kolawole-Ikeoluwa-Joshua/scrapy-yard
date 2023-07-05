c9fd6f73234069ee579c233aae59d036dff00085
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-07-05 08:19:01 +0100

    code reuse with interfaces

--- docs/interfaces.ts
+++ docs/interfaces.ts
@@ -1,9 +1,14 @@
-// interface
+// Interfaces
+// writing reusable code using typescript interfaces
 
+// we can use a single interface to describe different properties of different objects
+
+//singe interface
 interface Reportable {
     summary(): string;
 }
 
+// two different objects
 const oldCivic = {
     name: 'civic',
     year: new Date(),
@@ -13,11 +18,20 @@ const oldCivic = {
     }
 };
 
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`;
+    }
+};
 
-
+// different objects can interact with different functions
 const printSummary = (item: Reportable): void => {
     console.log(item.summary());
 
 };
 
 printSummary(oldCivic);
+printSummary(drink);
